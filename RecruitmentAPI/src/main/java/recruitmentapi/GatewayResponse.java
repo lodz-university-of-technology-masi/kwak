79ae239d16e167f875a16260d4e361e401cdae23
th7nder <vesphy@gmail.com>
2019-10-20 14:13:41 +0200

    Add explicit serialization to GatewayResponse

--- RecruitmentAPI/src/main/java/recruitmentapi/GatewayResponse.java
+++ RecruitmentAPI/src/main/java/recruitmentapi/GatewayResponse.java
@@ -1,5 +1,8 @@
 package recruitmentapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,21 +11,27 @@ import java.util.Map;
  * POJO containing response object for API Gateway.
  */
 public class GatewayResponse<Response> {
-
-    private final Response body;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private String body;
     private final Map<String, String> headers;
-    private final int statusCode;
+    private int statusCode;
 
     public GatewayResponse(final Response body, final int statusCode) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
-
         this.statusCode = statusCode;
-        this.body = body;
         this.headers = Collections.unmodifiableMap(new HashMap<>(headers));
+
+        try {
+            this.body = objectMapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            this.body = "Couldnt serialize Response object to JSON";
+            this.statusCode = 500;
+        }
+
     }
 
-    public Response getBody() {
+    public String getBody() {
         return body;
     }
 
--- RecruitmentAPI/src/main/java/recruitmentapi/GetTests.java
+++ RecruitmentAPI/src/main/java/recruitmentapi/GetTests.java
@@ -6,12 +6,18 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.util.List;
+import java.util.UUID;
 
 public class GetTests implements RequestHandler<Object, GatewayResponse<List<Test>>> {
     DynamoDBMapper mapper = new DynamoDBAdapter().getMapper();
 
     @Override
     public GatewayResponse<List<Test>> handleRequest(Object input, Context context) {
+        Test test = new Test();
+        test.setId(UUID.randomUUID());
+        test.setTitle(UUID.randomUUID().toString());
+        mapper.save(test);
+
         List<Test> tests = mapper.scan(Test.class, new DynamoDBScanExpression());
         return new GatewayResponse<>(tests, 200);
     }
