5d03a547682f5fab23ebfe305e22b23ede07e4f2
Kamil <kamil12p@gmail.com>
2019-12-04 19:19:42 +0100

    working layout

--- frontend/src/components/pages/candidate/CandidateHome.js
+++ frontend/src/components/pages/candidate/CandidateHome.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {API, Auth} from "aws-amplify";
 import {Route, Switch, Link} from "react-router-dom";
 import {Test} from "../Test";
-
 export default class CandidateHome extends Component {
     _isMounted = false;
 
@@ -35,58 +34,59 @@ export default class CandidateHome extends Component {
     componentWillUnmount() {
         this._isMounted = false;
     }
-
     render() {
         const {candidateTests, loading} = this.state;
         return (
-            <Switch>
-                <Route path="/tests/:testId" component={Test}/>
-                <Route path="/">
-                    <div className="container d-flex p-3 flex-column ">
-                        <header className="mb-3">
-                            <nav className="navbar navbar-light">
-                                <a className="navbar-brand">Recruiter</a>
-                                {loading && <div className="d-flex justify-content-center">
-                                    <div className="spinner-border" role="status">
-                                        <span className="sr-only">Loading...</span>
-                                    </div>
-                                </div>}
-                            </nav>
-                        </header>
+                <div className="container d-flex p-3 flex-column ">
+                    <header className="mb-3">
+                        <nav className="navbar navbar-light">
+                            <a className="navbar-brand">Recruiter</a>
+                            {loading && <div className="d-flex justify-content-center">
+                                <div className="spinner-border" role="status">
+                                    <span className="sr-only">Loading...</span>
+                                </div>
+                            </div>}
+                        </nav>
+                    </header>
 
-                        <main role="main">
-                            <div className="table-responsive">
-                                <table className="table table-striped">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">ID</th>
-                                        <th scope="col">Result</th>
-                                        <th scope="col" className="text-right"/>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    {candidateTests.map(candidateTest => (
+                    <main role="main">
+                        <Switch>
+                            <Route path="/tests/:testId" component={Test}/>
+                            <Route path="/">
+                                <div className="table-responsive">
+                                    <table className="table table-striped">
+                                        <thead>
                                         <tr>
-                                            <td><input type="text" className="form-control" id="testId" value={candidateTest.testId} disabled/></td>
-                                            <td><input type="text" className="form-control" id="result" disabled/></td>
-                                            <td className="text-right">
-                                                <Link to={`/tests/${candidateTest.testId}`}>
-                                                    <button id="solveButton" type="button" className="btn btn-secondary">Solve</button>
-                                                </Link>
-                                            </td>
+                                            <th scope="col">ID</th>
+                                            <th scope="col">Result</th>
+                                            <th scope="col" className="text-right"/>
                                         </tr>
+                                        </thead>
+                                        <tbody>
+                                        {candidateTests.map(candidateTest => (
+                                            <tr>
+                                                <td><input type="text" className="form-control" id="testId" value={candidateTest.testId}
+                                                           disabled/></td>
+                                                <td><input type="text" className="form-control" id="result" disabled/></td>
+                                                <td className="text-right">
+                                                    <Link to={`/tests/${candidateTest.testId}`}>
+                                                        <button id="solveButton" type="button" className="btn btn-secondary">Solve</button>
+                                                    </Link>
+                                                </td>
+                                            </tr>
                                         ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </main>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </Route>
 
-                        <footer>
-                            Recruiter v0.1
-                        </footer>
-                    </div>
-                </Route>
-            </Switch>
+                        </Switch>
+                    </main>
+
+                    <footer>
+                        Recruiter v0.1
+                    </footer>
+                </div>
         );
     }
 }
