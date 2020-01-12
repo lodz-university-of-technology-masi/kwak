e77f4e81762436c2681cec5ee57f61373e156796
th7nder <vesphy@gmail.com>
2020-01-12 01:03:02 +0100

    Fix presigned endpoint

--- RecruitmentAPI/src/main/java/recruitmentapi/endpoints/GeneratePresignedURL.java
+++ RecruitmentAPI/src/main/java/recruitmentapi/endpoints/GeneratePresignedURL.java
@@ -4,13 +4,14 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import recruitmentapi.GatewayRequest;
 import recruitmentapi.GatewayResponse;
+import recruitmentapi.model.PresignedURL;
 import recruitmentapi.model.Test;
 import recruitmentapi.services.ServiceContainer;
 
-public class GeneratePresignedURL extends ServiceContainer implements RequestHandler<GatewayRequest, GatewayResponse<String>> {
+public class GeneratePresignedURL extends ServiceContainer implements RequestHandler<GatewayRequest, GatewayResponse<PresignedURL>> {
 
     @Override
-    public GatewayResponse<String> handleRequest(GatewayRequest input, Context context) {
-        return new GatewayResponse<>(s3Service.generatePresignedURL().toString(), 200);
+    public GatewayResponse<PresignedURL> handleRequest(GatewayRequest input, Context context) {
+        return new GatewayResponse<>(s3Service.generatePresignedURL(), 200);
     }
 }
---
+++ RecruitmentAPI/src/main/java/recruitmentapi/model/PresignedURL.java
@@ -0,0 +1,27 @@
+package recruitmentapi.model;
+
+public class PresignedURL {
+    private String key;
+    private String url;
+
+    public PresignedURL(String key, String url) {
+        this.key = key;
+        this.url = url;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
