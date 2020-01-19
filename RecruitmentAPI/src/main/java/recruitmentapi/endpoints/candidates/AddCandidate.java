1537c8dfba5a7a0b2aa5173a0742d5fb682aee62
th7nder <vesphy@gmail.com>
2020-01-19 17:03:45 +0100

    Fix Add candidate exception and string validation

--- RecruitmentAPI/src/main/java/recruitmentapi/endpoints/candidates/AddCandidate.java
+++ RecruitmentAPI/src/main/java/recruitmentapi/endpoints/candidates/AddCandidate.java
@@ -1,7 +1,9 @@
 package recruitmentapi.endpoints.candidates;
 
+import com.amazonaws.services.cognitoidp.model.UsernameExistsException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import recruitmentapi.endpoints.ErrorMessage;
 import recruitmentapi.endpoints.GatewayRequest;
 import recruitmentapi.endpoints.GatewayResponse;
 import recruitmentapi.model.Candidate;
@@ -11,6 +13,10 @@ public class AddCandidate extends ServiceContainer implements RequestHandler<Gat
     @Override
     public GatewayResponse<Candidate> handleRequest(GatewayRequest request, Context context) {
         Candidate candidate = request.getTypedBody(Candidate.class);
-        return new GatewayResponse<>(cognitoService.createCandidate(candidate), 200);
+        try {
+            return new GatewayResponse<>(cognitoService.createCandidate(candidate), 200);
+        } catch (UsernameExistsException e) {
+            return new GatewayResponse<>(new ErrorMessage(400, "Email is already in use by admin or candidate"));
+        }
     }
 }
